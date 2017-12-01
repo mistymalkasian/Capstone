892cd247b3f705342c3b089e617e7605cb8d2fe5
Misty Malkasian <andrewandmisty@gmail.com>
2017-12-01 15:03:37 -0600

    Changed the css to look pretty

---
+++ PaulyMacs/PaulyMacs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PaulyMacs.MvcApplication" Language="C#" %>
---
+++ PaulyMacs/PaulyMacs/Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PaulyMacs.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string EmailAddress { get; set; }
+
+        public int Balance { get; set; }
+
+    }
+}
