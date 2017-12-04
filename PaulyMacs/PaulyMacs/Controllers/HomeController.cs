a4f42cb56066405709d642e7c21895ce212d151e
Misty Malkasian <andrewandmisty@gmail.com>
2017-12-04 14:47:15 -0600

    The app is now able to send emails

--- PaulyMacs/PaulyMacs/Controllers/HomeController.cs
+++ PaulyMacs/PaulyMacs/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCEmail.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace PaulyMacs.Controllers
 {
@@ -25,6 +29,43 @@ namespace PaulyMacs.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Sent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailForm model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("andrewandmisty@gmail.com")); 
+                message.From = new MailAddress("devcodecamptest@gmail.com");
+                message.Subject = "Rate Us!";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "devcodecamptest@gmail.com",
+                        Password = "devCodeDawg02"
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ PaulyMacs/PaulyMacs/Models/EmailForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MVCEmail.Models
+    {
+        public class EmailForm
+        {
+            [Required, Display(Name = "Your name:")]
+            public string FromName { get; set; }
+            [Required, Display(Name = "Your email:"), EmailAddress]
+            public string FromEmail { get; set; }
+            [Required, Display(Name ="Your comments:")]
+            public string Message { get; set; }
+        }
+    }
