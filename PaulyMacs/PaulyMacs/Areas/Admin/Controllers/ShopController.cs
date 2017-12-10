04a3adac0abb4edcdb0701a9f9cb348faa009491
Misty Malkasian <andrewandmisty@gmail.com>
2017-12-09 22:46:47 -0600

    Admin can now delete a specific gallery image

--- PaulyMacs/PaulyMacs/Areas/Admin/Controllers/ShopController.cs
+++ PaulyMacs/PaulyMacs/Areas/Admin/Controllers/ShopController.cs
@@ -419,8 +419,8 @@ namespace PaulyMacs.Areas.Admin.Controllers
             return RedirectToAction("EditMenuItem");
         }
 
-
-       public ActionResult DeleteMenuItem(int id)
+        //GET: Admin/Shop/DeleteMenuItem/id
+        public ActionResult DeleteMenuItem(int id)
         {
             using (ApplicationDbContext db = new ApplicationDbContext())
             {
@@ -438,6 +438,47 @@ namespace PaulyMacs.Areas.Admin.Controllers
                 Directory.Delete(pathString, true);
 
             return RedirectToAction("MenuItems");
+        }
+
+        //POST: Admin/Shop/SaveGalleryImages
+        [HttpPost]
+        public void SaveGalleryImages(int id)
+        {
+            foreach(string fileName in Request.Files)
+            {
+                HttpPostedFileBase file = Request.Files[fileName];
+                if (file != null && file.ContentLength > 0)
+                {
+                    var originalDirectory = new DirectoryInfo(string.Format("{0}Images\\Uploads", Server.MapPath(@"\")));
+
+                    string pathString1 = Path.Combine(originalDirectory.ToString(), "MenuItems\\" + id.ToString() + "\\Gallery");
+                    string pathString2 = Path.Combine(originalDirectory.ToString(), "MenuItems\\" + id.ToString() + "\\Gallery\\Thumbs");
+
+                    var path = string.Format("{0}\\{1}", pathString1, file.FileName);
+                    var path2 = string.Format("{0}\\{1}", pathString2, file.FileName);
+
+                    file.SaveAs(path);
+                    WebImage img = new WebImage(file.InputStream);
+                    img.Resize(200, 200);
+                    img.Save(path2);
+                }
+
+            }
+
+        }
+
+        //POST: Admin/Shop/DeleteImage
+        [HttpPost]
+        public void DeleteImage(int id, string imageName)
+        {
+            string fullPath1 = Request.MapPath("~Images/Uploads/MenuItems/" + id.ToString() + "/Gallery/" + imageName);
+            string fullPath2 = Request.MapPath("~Images/Uploads/MenuItems/" + id.ToString() + "/Gallery/Thumbs/" + imageName);
+
+            if (System.IO.File.Exists(fullPath1))
+                System.IO.File.Delete(fullPath1);
+
+            if (System.IO.File.Exists(fullPath2))
+                System.IO.File.Delete(fullPath2);
         }
     }
 }
