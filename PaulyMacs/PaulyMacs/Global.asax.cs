b61ec6cd28300694d45bb58efcc5d623d7451216
Misty Malkasian <andrewandmisty@gmail.com>
2017-12-12 13:04:54 -0600

    Made add to cart only visible if someone is logged in

--- PaulyMacs/PaulyMacs/Global.asax.cs
+++ PaulyMacs/PaulyMacs/Global.asax.cs
@@ -20,27 +20,5 @@ namespace PaulyMacs
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
 
-        //protected void Application_AuthenticateRequest()
-        //{
-
-        //    if(User == null) { return; }
-
-        //    string username = Context.User.Identity.Name;
-
-        //    string[] roles = null;
-
-        //    using (ApplicationDbContext db = new ApplicationDbContext())
-        //    {
-        //        ApplicationUser user = db.Users.FirstOrDefault(x => x.UserName == username);
-
-        //        roles = db.Roles.Where(x => x.Id == user.Id).Select(x => x.Name).ToArray();
-
-        //    }
-        //    IIdentity userIdentity = new GenericIdentity(username);
-        //    IPrincipal newUserObj = new GenericPrincipal(userIdentity, roles);
-
-        //    Context.User = newUserObj;
-
-        //}
     }
 }
